c9826677dfd922545d95bc87125dfc9c9da7a0cc
Lucas <53381311+Lxcasx@users.noreply.github.com>
2022-07-04 20:33:01 +0200

    added theme provider

--- src/Client.Infrastructure/ApiClient/FSHApi.cs
+++ src/Client.Infrastructure/ApiClient/FSHApi.cs
@@ -5,7 +5,7 @@
 //----------------------
 
 
-using System = global::System;
+using System;
 
 #nullable enable
 
---
+++ src/Client/Shared/ThemeProvider.cs
@@ -0,0 +1,42 @@
+﻿using MudBlazor;
+using ReflectInput.Client.Infrastructure.Preferences;
+using ReflectInput.Client.Infrastructure.Theme;
+
+namespace ReflectInput.Client.Shared;
+
+public class ThemeProvider
+{
+    public ClientPreference? ThemePreference { get; private set; }
+    public MudTheme CurrentTheme { get; private set; } = new LightTheme();
+    public delegate void ThemePreferenceChangedHandler(MudTheme theme);
+    public event ThemePreferenceChangedHandler ThemePreferenceChanged;
+
+    public void Initialize(ClientPreference? clientPreference)
+    {
+        ThemePreference = clientPreference;
+
+        if (ThemePreference == null)
+        {
+            ThemePreference = new ClientPreference();
+        }
+
+        SetCurrentTheme(ThemePreference);
+    }
+
+    public void ChangeThemePreference(ClientPreference themePreference)
+    {
+        SetCurrentTheme(themePreference);
+        ThemePreferenceChanged?.Invoke(CurrentTheme);
+        //await ClientPreferences.SetPreference(themePreference);
+    }
+
+    private void SetCurrentTheme(ClientPreference themePreference)
+    {
+        CurrentTheme = themePreference.IsDarkMode ? new DarkTheme() : new LightTheme();
+        CurrentTheme.Palette.Primary = themePreference.PrimaryColor;
+        CurrentTheme.Palette.Secondary = themePreference.SecondaryColor;
+        CurrentTheme.LayoutProperties.DefaultBorderRadius = $"{themePreference.BorderRadius}px";
+        CurrentTheme.LayoutProperties.DefaultBorderRadius = $"{themePreference.BorderRadius}px";
+        //_rightToLeft = themePreference.IsRTL;
+    }
+}
